2246e2c66cf1d732a279367e462746a18accd5e5
unknown <lastsaintcrucified@gmail.com>
2019-06-20 07:02:44 +0600

    First commit

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id : 1,
+        title : 'Project1',
+        description : 'A react project involving js and other web dev tool and very much done with care',
+        link : 'https://www.google.com',
+        image : project1
+    },
+    {
+        id : 2,
+        title : 'Project2',
+        description : 'A react project to create library and very much done with care',
+        link : 'https://www.google.com',
+        image : project2
+    },
+    {
+        id : 3,
+        title : 'Project3',
+        description : 'A react project to demonstrate important contact no and very much done with care',
+        link : 'https://www.google.com',
+        image : project3
+    }
+];
+export default PROJECTS;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,32 @@
+import email from '../assets/email_icon.png';
+import github from '../assets/github_icon.png';
+import linkedin from '../assets/linkedin_icon.png';
+import twitter from '../assets/twitter_icon.png';
+
+const PROFILES = [
+    {
+        id:1,
+        image:email,
+        title:'Email',
+        link:'https://lastsaintcrucified@gmail.com'
+    },
+    {
+        id:2,
+        image:github,
+        title:'Github',
+        link:'https://github.com/lastsaintcrucified/node-course-web-server'
+    },
+    {
+        id:3,
+        image:linkedin,
+        title:'LinkedIn',
+        link:'https://www.linkedin.com/in/md-towhidul-islam-bhy-a6a814158'
+    },
+    {
+        id:4,
+        image:twitter,
+        title:'Twitter',
+        link:'https://www.twitter.com'
+    }
+];
+export default PROFILES;
